8ea000c156070e80c15808d34a077d00cdf0e311
Saharrock <salnikovsergej76@gmail.com>
2025-11-04 14:44:47 +0200

    added reset buttons

--- ViewModels/LogInPageViewModel.cs
+++ ViewModels/LogInPageViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace Soulbound.ViewModels
 {
@@ -62,11 +63,56 @@ namespace Soulbound.ViewModels
                 OnPropertyChanged();
             }
         }
+
+        public string userPassword;
+        public string UserPassword
+        {
+            get { return userPassword; }
+            set
+            {
+                userPassword = value;
+                OnPropertyChanged();
+            }
+            
+        }
         #endregion
 
-        // constructor
+        #region Commands
+        public ICommand ResetUsernameCommand { get; set; }
+        public ICommand ResetPasswordCommand { get; set; }
+        public ICommand GotoAnotherPageCommand { get; set; }
+        #endregion
+
+        #region constructor
         public LogInPageViewModel()
         {
+            // Defining the Command for a non async Function
+            ResetUsernameCommand = new Command(ResetUserField);
+            ResetPasswordCommand = new Command(ResetPasswordField);
+            // Defining the Command for an async Function
+            GotoAnotherPageCommand = new Command(async () => await GotoAnotherPage());
         }
+        #endregion
+
+        #region  Methods
+        private void ResetUserField()
+        {
+            UserInput = "";
+            MessageForUser = "";
+
+        }
+
+        private void ResetPasswordField()
+        {
+            UserPassword = "";
+            MessageForUser = "";
+        }
+        private async Task GotoAnotherPage()
+        {
+            MainPage mp = new MainPage();
+            await Application.Current.MainPage.Navigation.PushAsync(mp);
+
+        }
+        #endregion
     }
 }
